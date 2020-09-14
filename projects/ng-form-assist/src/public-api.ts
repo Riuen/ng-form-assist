889e6f3ee2b89e405e014cbba8fbfb00092d3acf
rgordon <rgordon@egovja.com>
2020-09-14 13:01:43 -0500

    export smart field directive and custom validators

--- projects/ng-form-assist/src/public-api.ts
+++ projects/ng-form-assist/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/ng-form-assist.service';
 export * from './lib/ng-form-assist.component';
 export * from './lib/ng-form-assist.module';
+export * from './lib/smart-field/smart-field.directive';
+export * from './lib/utils/custom-validators';
