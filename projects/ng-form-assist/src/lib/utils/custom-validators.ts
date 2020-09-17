515cf1810f6dc985e5a31532a6608d927e86ebb7
Riuen <riuen@protonmail.com>
2020-09-16 19:53:37 -0500

    removed unsupported validators

--- projects/ng-form-assist/src/lib/utils/custom-validators.ts
+++ projects/ng-form-assist/src/lib/utils/custom-validators.ts
@@ -68,68 +68,6 @@ export class CustomValidators {
         }
     }
 
-    public static validateEmail(fc: AbstractControl) {
-
-        const email = fc.value as string;
-
-        if (email) {
-
-          return (email.match(/^[ ]*[^ @\t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+[ ]*$/))
-            ? null
-            : { validateEmail: {valid: false}};
-        }
-
-        return null;
-    }
-
-    public static validatePhoneNumber(fc: AbstractControl) {
-
-        const phoneNumber = fc.value as string;
-
-        if (phoneNumber) {
-
-          return (phoneNumber.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/))
-            ? null
-            : { validateEmail: {valid: false}};
-        }
-
-        return null;
-    }
-
-
-
-    /* For cases where you may want to do your own validation from
-        outside the component. Eg calling an API in order to validate some input;
-        This validator exists as a sort of placeholder, you can specify a custom message
-        and manually set the error in accordance with your logic to have the message displayed
-        and error status of the field reflected
-        TODO - Reword this hot garbage, possible rename function.
-    */
-    public static manualValidation(formCtrl: AbstractControl) {
-        return formCtrl.hasError('manualValidation')
-          ? { manualValidation: {valid: false} } : null;
-    }
-
-    public static removeManualValidationError(fieldCtrl: AbstractControl) {
-
-        let fieldErrors = fieldCtrl.errors;
-
-        if (fieldCtrl.errors) {
-          delete fieldErrors?.manualValidation;
-        }
-
-        fieldErrors = (Object.keys(fieldErrors || {}).length >= 1) ? fieldErrors : null;
-        fieldCtrl.setErrors(fieldErrors);
-    }
-
-    public static addManualValidationError(fieldCtrl: AbstractControl) {
-
-        const fieldErrors = fieldCtrl.errors || {};
-        fieldErrors.manualValidation = {valid: false};
-
-        fieldCtrl.setErrors(fieldErrors);
-    }
-
     public static validatePasswordComplexity(fieldCtrl: AbstractControl) {
 
         const password = fieldCtrl.value as string;
